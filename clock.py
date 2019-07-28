728ec6f3536fbad42340215f5cd2b8e9478b717c
shlomioved1234 <shlomioved123@gmail.com>
2019-07-28 19:48:58 -0400

    add interval job

--- clock.py
+++ clock.py
@@ -1,13 +1,28 @@
 import requests
 import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 def send_text(message):
-    resp = requests.post('https://textbelt.com/text', {
-                         'phone': '5166039008',
-                         'message': message,
-                         'key': 'textbelt',
-                         })
+    email = "unibidinc@gmail.com"
+    pas = "Unibid123"
+    sms_gateway = '+15166039008@tmomail.net'
+    smtp = "smtp.gmail.com"
+    port = 587
+    server = smtplib.SMTP(smtp,port)
+    server.starttls()
+    server.login(email,pas)
+    msg = MIMEMultipart()
+    msg['From'] = email
+    msg['To'] = sms_gateway
+    msg['Subject'] = "JEWISH ZMANIM!!!!"
+    body = message
+    msg.attach(MIMEText(body, 'plain'))
+    sms = msg.as_string()
+    server.sendmail(email,sms_gateway,sms)
+    server.quit()
 
 sched = BlockingScheduler()
 
@@ -21,6 +36,15 @@ def scheduled_job():
         message += key + ': ' + value + '\n'
     send_text(message)
 
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    d = datetime.datetime.today()
+    today = d.strftime('%m/%d/%Y')
+    jewish_times = requests.get("https://wyrezmanim.herokuapp.com/api/zmanim?timezone=America/New_York&latitude=40&longitude=-73&date=%s&elevation=50&format=json" % (today))
+    message = ''
+    for key, value in jewish_times.json().items():
+        message += key + ': ' + value + '\n'
+    send_text(message)
 sched.start()
 
 
